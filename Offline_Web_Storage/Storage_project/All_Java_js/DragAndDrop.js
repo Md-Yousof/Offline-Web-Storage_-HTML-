c49c51ef9f302350de1b5268cd459313d3f5a8d9
Mohammed Yousof <135439769+Md-Yousof@users.noreply.github.com>
2023-09-27 23:37:13 +0600

    Add files via upload

---
+++ Offline_Web_Storage/Storage_project/All_Java_js/DragAndDrop.js
@@ -0,0 +1,42 @@
+﻿  
+   
+        $(document).ready(function () {
+            var filesTable = $('#filesTable');
+        var target = $('#target');
+
+        target.on('dragover', function (e) {
+            e.preventDefault();
+        target.addClass('dragging');
+            });
+
+        target.on('dragleave', function (e) {
+            e.preventDefault();
+        target.removeClass('dragging');
+            });
+
+        target.on('drop', function (e) {
+            e.preventDefault();
+        target.removeClass('dragging');
+
+        $.each(e.originalEvent.dataTransfer.files, function (index, file) {
+                    var newRow = $('<tr>');
+
+            var nameCell = $('<td>').text(file.name);
+                var typeCell = $('<td>').text(file.type);
+                    var sizeCell = $('<td>').text(file.size);
+
+                        var downloadLink = $('<a>').text('Download');
+                            downloadLink.attr('href', URL.createObjectURL(file));
+                            downloadLink.attr('download', file.name);
+                            var downloadCell = $('<td>').append(downloadLink);
+
+                                newRow.append(nameCell);
+                                newRow.append(typeCell);
+                                newRow.append(sizeCell);
+                                newRow.append(downloadCell);
+
+                                filesTable.append(newRow);
+                });
+            });
+        });
+              
---
+++ Offline_Web_Storage/Storage_project/All_Java_js/IndexedDB.js
@@ -0,0 +1,106 @@
+﻿/// <reference path="../script_jquery/jquery-3.1.1.min.js" />
+
+
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+window.IDBCursor = window.IDBCursor || window.webkitIDBCursor;
+
+$(document).ready(function () {
+    contactsNamespace.initialize();
+});
+
+(function () {
+    this.contactsNamespace = this.contactsNamespace || {};
+    var ns = this.contactsNamespace;
+    var currentRecord;
+    var db;
+
+    ns.initialize = function () {
+        $("#btnSave").on('click', ns.save);
+        var request = indexedDB.open("myDB_R53", 1);
+
+        request.onupgradeneeded = function (response) {
+            var options = { keypath: "id", autoIncrement: true };
+            response.currentTarget.result.createObjectStore("contacts", options);
+        }
+        request.onsuccess = function (response) {
+            db = request.result;
+            ns.display();
+        }
+    }
+
+    ns.display = function () {
+        $("#currentAction").html('Add Contact');
+        currentRecord = { key: null, contact: {} };
+        displayCurrentRecord();
+
+        var trans = db.transaction('contacts', 'readonly');
+        var request = trans.objectStore("contacts").openCursor();
+        var results = [];
+
+        request.onsuccess = function (response) {
+            var cursor = response.target.result;
+            if (!cursor) {
+                bindToGrid(results);
+                return;
+            }
+            results.push({ key: cursor.key, contact: cursor.value });
+            cursor.continue();
+        }
+    }
+
+    function bindToGrid(results) {
+        var html = "";
+        for (var i = 0; i < results.length; i++) {
+            var key = results[i].key;
+            var contact = results[i].contact;
+
+            html += '<tr><td>' + contact.email + '</td>';
+            html += '<td>' + contact.firstName + ' ' + contact.lastName + '</td>';
+            html += '<td><a class="edit" href="javascript:void(0)" data-key=' + key + '>Edit</a></td></tr>';
+        }
+        html = html || '<tr><td colspan="3">No Records Available!!!</td></tr>'
+        $("#contacts tbody").html(html);
+        $('#contacts a.edit').on('click', ns.loadContact);
+    }
+
+    ns.loadContact = function () {
+        var key = parseInt($(this).attr('data-key'));
+        var trans = db.transaction('contacts', 'readonly');
+        var store = trans.objectStore("contacts");
+        var request = store.get(key);
+
+        request.onsuccess = function (response) {
+            $("#currentAction").html('Edit Contact');
+            currentRecord = { key: key, contact: response.target.result }
+            displayCurrentRecord();
+        }
+    }
+
+    function displayCurrentRecord() {
+        var contact = currentRecord.contact;
+        $("#firstName").val(contact.firstName);
+        $("#lastName").val(contact.lastName);
+        $("#email").val(contact.email);
+        $("#phoneNumber").val(contact.phoneNumber);
+    }
+
+    ns.save = function () {
+        var contact = currentRecord.contact;
+        contact.firstName = $("#firstName").val();
+        contact.lastName = $("#lastName").val();
+        contact.email = $("#email").val();
+        contact.phoneNumber = $("#phoneNumber").val();
+
+        var trans = db.transaction('contacts', 'readwrite');
+        var contacts = trans.objectStore("contacts");
+        var request = currentRecord.key != null
+            ? contacts.put(contact, currentRecord.key)
+            : contacts.add(contact);
+
+        request.onsuccess = function (response) {
+            ns.display();
+        }
+    }
+
+})();
